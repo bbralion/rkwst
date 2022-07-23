a570c0c084e51e8373e85e4fa52fa1cdb706eaf7
olefirenque <egor.olefirenko892@gmail.com>
2022-07-24 00:17:10 +0300

    feat(api): add migration script for request table

---
+++ api/priv/repo/migrations/20220723175237_create_request_table.exs
@@ -0,0 +1,21 @@
+defmodule Rkwst.Repo.Migrations.CreateRequestTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:requests) do
+      add :ip, :string
+      add :proto, :string
+      add :timestamp, :string
+      add :method, :string
+      add :uri, :string
+      add :headers, :map
+      add :form, :map
+      add :body, :string
+      add :bin_id, references(:bins, on_delete: :delete_all)
+
+      timestamps
+    end
+
+    create index(:requests, [:bin_id])
+  end
+end
